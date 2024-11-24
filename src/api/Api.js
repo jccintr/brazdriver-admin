76bd74294d71e981554bfc46731c021f8568f5ba
Julio Cesar <jccintr@gmail.com>
2024-11-24 20:02:56 -0300

    alterado api js

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,9 +1,9 @@
 // --host=192.168.0.107
 
 
-//const BASE_API = 'https://taxibraz.onrender.com';
+const BASE_API = 'https://taxibraz.onrender.com';
 //const BASE_API = 'http://192.168.0.108:3000';
-const BASE_API = 'http://192.168.1.8:3000';
+//const BASE_API = 'http://192.168.1.8:3000';
 
 
 
