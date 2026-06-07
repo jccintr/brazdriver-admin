75990bcf9beb4a119c8b91f6c9ee21aac7805e6c
Julio Cesar <jccintr@gmail.com>
2026-06-07 19:12:27 -0300

    ajustado GridDrivers e GridPassengers

--- src/components/grids/GridDrivers.jsx
+++ src/components/grids/GridDrivers.jsx
@@ -2,7 +2,7 @@ import CardDriver from '../cards/CardDriver'
 
 const GridDrivers = ({drivers,onView,toggleDriverStatus}) => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 2xl:grid-cols-4 gap-4 pt-4 pb-4 mx-auto'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2  xl:grid-cols-3 2xl:grid-cols-4 gap-4 pt-4 pb-4 mx-auto'>
             {drivers.map((driver)=><CardDriver toggleDriverStatus={toggleDriverStatus}key={driver._id} driver={driver} onView={onView}/>)}
     </div>
   )
--- src/components/grids/GridPassengers.jsx
+++ src/components/grids/GridPassengers.jsx
@@ -4,7 +4,7 @@ import CardPassenger from '../cards/CardPassenger';
 
 const GridPassengers = ({passengers,onView}) => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 2xl:grid-cols-4 gap-4 pt-4 pb-4 mx-auto'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2  xl:grid-cols-3 2xl:grid-cols-4 gap-4 pt-4 pb-4 mx-auto'>
             {passengers.map((passenger)=><CardPassenger key={passenger._id} passenger={passenger} onView={onView}/>)}
     </div>
   )
